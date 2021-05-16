40397353abf02523d3a19391a45bff97fcc45d59
lkoteuka <llkot3ll@mail.ru>
2021-05-16 11:21:01 +0300

    Added base logic, field and constants

---
+++ handlers.py
@@ -0,0 +1,13 @@
+def bomb():
+    print("YOU ARE DEAD!!!")
+
+
+def none():
+    print("Nothing")
+
+
+def number(num):
+    def print_number():
+        print("Number ", num)
+
+    return print_number
