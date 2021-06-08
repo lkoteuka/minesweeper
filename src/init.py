f43ecabca3e33b7769c27071d2b18202bfa09c9e
Mikhail_Timoshkin <timoshkin.michail@gmail.com>
2021-06-08 21:58:22 +0300

    Добавлены тесты

--- src/init.py
+++ src/init.py
@@ -6,11 +6,13 @@ def initialize(size_x=const.HEIGHT, size_y=const.WIDTH, bomb_number=const.BOMBS)
     """
     Function initializes GUI, sets field's sizes, places bombs, sets user controls.
 
+    :param bomb_number: number of bobs
+    :type bomb_number: int
     :param size_x: length of map in cells
     :type size_x: int
     :param size_y: width of map in cells
     :type size_y: int
     """
-    field = gui.FieldFrame(gui.root, cols=size_y, rows=size_x)
+    field = gui.FieldFrame(gui.root, cols=size_y, rows=size_x, bomb_number=bomb_number)
     gui.TopFrame(gui.root, cols=size_y, field_restart=field.restart)
     gui.root.mainloop()
